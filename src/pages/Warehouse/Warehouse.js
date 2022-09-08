29e3b53b8e1c7b4612e3e7e9ddcfb2909022fac9
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-09-07 22:44:04 -0600

    clean up

--- src/pages/Warehouse/Warehouse.js
+++ src/pages/Warehouse/Warehouse.js
@@ -9,19 +9,6 @@ function Warehouse () {
     return (
       <>
       <WarehouseHeader />
-      {/* TODO add a note here to wrap the edit button in a link component */}
-      <ul>
-        <Link
-          to={`editWarehouse/id1`}
-        >
-          <li>1</li>
-        </Link>
-        <Link
-          to={`editWarehouse/id2`}
-        >
-          <li>2</li>
-        </Link>
-      </ul>
       </>
     );
   }
