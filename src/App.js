0c1ebed226f60d3d0875c6db582193c7cab8c0ed
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-09-07 22:40:50 -0600

    add route to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-// import './styles/global.scss'
-
- 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Warehouse from './pages/Warehouse/Warehouse';
 import Inventory from './pages/Inventory/Inventory';
 import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
-import WarehouseHeader from "./components/Header/WarehouseHeader";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-          <WarehouseHeader />
           <Routes>
             <Route path="/" element={<Warehouse />} />
             <Route path="/inventory" element={<Inventory />} />
             <Route path="/editWarehouse/:warehouseId" element={<EditWarehouse />} />
           </Routes>
       </BrowserRouter>
-      </>
+    </>
   );
 }
 
