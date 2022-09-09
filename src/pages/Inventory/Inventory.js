574ffc80afcf8ca13a533a1d2b185fd93b15baac
Ados4453 <matthawkins44@gmail.com>
2022-09-09 15:45:19 -0500

    fixed iventory header search field styles across all breakpoints

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -2,54 +2,69 @@ import './Inventory.scss';
 //icons
 import Search from '../../assets/Icons/search-24px.svg';
 import Sort from '../../assets/Icons/sort-24px.svg'
+import Invin from '../../assets/data/inventories.json'
+
+import { useState, useEffect } from "react"
+import axios from 'axios';
 
 
 
 function Inventory () {
-
+  // const [currWarehouse, setCurrWarehouse] = useState(null)
+  // // console.log(currWarehouse)
+  // useEffect(()=> {
+  //   axios.get(`http://localhost:8080/inventory`)
+  //   .then((res) =>{
+  //     console.log(res.data)
+  //     // setCurrWarehouse(res)
+  //   })
+  // }, [])
   
+
     return (
-      <div className='invientory'>
-        <div className='invientory__head'>
-          <h1 className='invientory__title'>Inventory</h1>
-          <div className='invientory__ui-head'>
-            <div className='invientory__search'>
-              <input className='invientory__input' type="text" size='8' placeholder='Search...'></input>
-              <img className='invientory__s-icon' src={Search} alt="magnifying glass"></img>
-            </div>
-            <div className='invientory__add-btn'>
-              <button className='invientory__btn'>+ Add New Item</button>
+      <div className='container'>
+        <div className='invientory'>
+          <div className='invientory__head'>
+            <h1 className='invientory__title'>Inventory</h1>
+            <div className='invientory__ui-head'>
+              <div className='invientory__search'>
+                <input className='invientory__input' type="text" size='8' placeholder='Search...'></input>
+                <img className='invientory__s-icon' src={Search} alt="magnifying glass"></img>
+              </div>
+              <div className='invientory__add-btn'>
+                <button className='invientory__btn'>+ Add New Item</button>
+              </div>
             </div>
           </div>
+          <section className='invientory__header-catagory'>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>INVENTORY ITEM</h3>
+              <img className='invientory__cat-pic' src={Sort}></img>
+            </div>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>CATAGORY</h3>
+              <img className='invientory__cat-pic' src={Sort}></img>
+            </div>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>STATUS</h3>
+              <img className='invientory__cat-pic' src={Sort}></img>
+            </div>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>QTY</h3>
+              <img className='invientory__cat-pic' src={Sort}></img>
+            </div>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>WAREHOUSE</h3>
+              <img className='invientory__cat-pic' src={Sort}></img>
+            </div>
+            <div className='invientory__header-cat'>
+              <h3 className='invientory__cat-i'>ACTION</h3>
+            </div>
+          </section>
+          <section className='invientory__list'>
+            
+          </section>
         </div>
-        <section className='invientory__header-catagory'>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>INVENTORY ITEM</h3>
-            <img className='invientory__cat-pic' src={Sort}></img>
-          </div>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>CATAGORY</h3>
-            <img className='invientory__cat-pic' src={Sort}></img>
-          </div>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>STATUS</h3>
-            <img className='invientory__cat-pic' src={Sort}></img>
-          </div>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>QTY</h3>
-            <img className='invientory__cat-pic' src={Sort}></img>
-          </div>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>WAREHOUSE</h3>
-            <img className='invientory__cat-pic' src={Sort}></img>
-          </div>
-          <div className='invientory__header-cat'>
-            <h3 className='invientory__cat-i'>ACTION</h3>
-          </div>
-        </section>
-        <section className='invientory__list'>
-      
-        </section>
       </div>
     );
   }
