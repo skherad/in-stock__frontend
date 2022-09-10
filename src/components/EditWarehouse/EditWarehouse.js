e8fdb0eb61dac5de439a288b58d5419c8c465ddb
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-09-10 09:34:31 -0600

    state correction

--- src/components/EditWarehouse/EditWarehouse.js
+++ src/components/EditWarehouse/EditWarehouse.js
@@ -4,34 +4,44 @@ import './EditWarehouse.scss';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
 const EditWarehouse = () => {
 
     let navigate = useNavigate();
 
-    //TODO click will need to link to /:warehouseId coordinate with Michael
     const {warehouseId} = useParams();
 
-    //define state for selected warehouse to be edited
-    const [selectedWarehouse, setSelectedWarehouse] = useState();
     //define states for handling change of input values - to be used to set edited values
-    const [state, setState] = useState({
-        name: selectedWarehouse?.name,
-        address: selectedWarehouse?.address,
-        city: selectedWarehouse?.city,
-        country: selectedWarehouse?.country,
+    const [state, setState] = useState({ 
+        name: "",
+        address: "",
+        city: "",
+        country: "",
         contact: {
-            name: selectedWarehouse?.contact.name,
-            position: selectedWarehouse?.contact.position,
-            phone: selectedWarehouse?.contact.phone,
-            email: selectedWarehouse?.contact.email
-        }
-    });
+            name: "",
+            position: "",
+            phone: "",
+            email: ""
+        }});
+
     //get request to receive and set selected warehouse details
     useEffect(() => {
-        axios.get(`http://localhost:8080/warehouse/${warehouseId}`)
-        .then(response => setSelectedWarehouse(response.data))
+        axios.get(`${API_URL}/warehouse/${warehouseId}`)
+        .then(response => setState({
+            name: response.data.name,
+            address: response.data.address,
+            city: response.data.city,
+            country: response.data.country,
+            contact: {
+                name: response.data.contact.name,
+                position: response.data.contact.position,
+                phone: response.data.contact.phone,
+                email: response.data.contact.email
+            }
+        }))
         .catch(error => console.log(error))
-    }, [warehouseId])
+    }, [])
 
     //change handler for warehoue details from user inputs
     const handleChangeWarehouse = (event) => {
@@ -41,7 +51,7 @@ const EditWarehouse = () => {
           [event.target.name]: value,
         })
     }
-
+    //change handler for contact details from user inputs
     const handleChangeContact = (event) => {
         const value = event.target.value;
         setState({
@@ -55,7 +65,7 @@ const EditWarehouse = () => {
 
     //click handler for cancel button
     const handleCancel = () => {
-        navigate('/warehouseDetails')
+        navigate(`/warehouse/${warehouseId}`)
     }
 
     //click handler for submitting the form
@@ -63,19 +73,20 @@ const EditWarehouse = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(state)
-        // axios.put(`http://localhost:8080/warehouse/${warehouseId}`, state)
-        // .then(response=>console.log(response))
-        // .catch(error=>console.log(error))
+        if(!state.name || !state.address || !state.city || !state.country || !state.contact.name || !state.contact.position || !state.contact.phone || !state.contact.email) {
+            return;
+        } else {
+        axios.put(`${API_URL}/warehouse/${warehouseId}`, state)
+        .then(response=>console.log(response))
+        .catch(error=>console.log(error))}
     }
-
-    if(!selectedWarehouse) { return <div>loading...</div>}
 
     return (
     <div className='parent-container'>
     <section className="form__container">
         <div className='form__header-container'>
             <Link 
-                to={`/warehouseDetails`}
+                to={`/warehouse/${warehouseId}`}
                 className="form__icon"
             >
                 <img src={BackArrow} alt="arrow back"></img>
@@ -94,11 +105,12 @@ const EditWarehouse = () => {
                     >Warehouse Name</label>
                     <input 
                         name="name"
-                        className="form__input"
-                        placeholder={selectedWarehouse.name}
-                        value={state.name}
+                        className={!state?.name?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.name}
+                        value={state?.name}
                         onChange={handleChangeWarehouse}
                     ></input>
+                    <div className={!state?.name?"invalid": "valid"}>This field is required</div>
 
                     {/* warehouse address input field */}
                     <label 
@@ -107,12 +119,13 @@ const EditWarehouse = () => {
                     >Street Address</label>
                     <input 
                         name="address"
-                        className="form__input"
-                        placeholder={selectedWarehouse.address}
-                        value={state.address}
+                        className={!state?.address?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.address}
+                        value={state?.address}
                         onChange={handleChangeWarehouse}
                     ></input>
-
+                    <div className={!state?.address?"invalid": "valid"}>This field is required</div>
+                    
                     {/* warehouse city input field */}
                     <label 
                         htmlFor="city"
@@ -120,12 +133,13 @@ const EditWarehouse = () => {
                     >City</label>
                     <input 
                         name="city"
-                        className="form__input"
-                        placeholder={selectedWarehouse.city}
-                        value={state.city}
+                        className={!state?.city?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.city}
+                        value={state?.city}
                         onChange={handleChangeWarehouse}
                     ></input>
-
+                    <div className={!state?.city?"invalid": "valid"}>This field is required</div>
+                    
                     {/* warehouse country input field */}
                     <label 
                         htmlFor="country"
@@ -133,11 +147,12 @@ const EditWarehouse = () => {
                     >Country</label>
                     <input 
                         name="country"
-                        className="form__input"
-                        placeholder={selectedWarehouse.country}
-                        value={state.country}
+                        className={!state?.country?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.country}
+                        value={state?.country}
                         onChange={handleChangeWarehouse}
                     ></input>
+                    <div className={!state?.country?"invalid": "valid"}>This field is required</div>
                 </section>
                 <section className="form__section form__section--divider">
                     <h2 className="form__title">Contact Details</h2>
@@ -149,11 +164,12 @@ const EditWarehouse = () => {
                     >Contact Name</label>
                     <input 
                         name="name"
-                        className="form__input"
-                        placeholder={selectedWarehouse.contact.name}
-                        value={state.contact.name}
+                        className={!state?.contact.name?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.contact.name}
+                        value={state?.contact.name}
                         onChange={handleChangeContact}
                     ></input>
+                    <div className={!state?.contact.name?"invalid": "valid"}>This field is required</div>
 
                     {/* contact position input field */}
                     <label 
@@ -162,11 +178,12 @@ const EditWarehouse = () => {
                     >Position</label>
                     <input 
                         name="contactPosition"
-                        className="form__input"
-                        placeholder={selectedWarehouse.contact.position}
-                        value={state.contact.position}
+                        className={!state?.contact.position?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.contact.position}
+                        value={state?.contact.position}
                         onChange={handleChangeContact}
                     ></input>
+                    <div className={!state?.contact.position?"invalid": "valid"}>This field is required</div>
 
                     {/* contact phone# input field */}
                     <label 
@@ -175,11 +192,12 @@ const EditWarehouse = () => {
                     >Phone Number</label>
                     <input 
                         name="phone"
-                        className="form__input"
-                        placeholder={selectedWarehouse.contact.phone}
-                        value={state.contact.phone}
+                        className={!state?.contact.phone?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.contact.phone}
+                        value={state?.contact.phone}
                         onChange={handleChangeContact}
                     ></input>
+                    <div className={!state?.contact.phone?"invalid": "valid"}>This field is required</div>
 
                     {/* contact email input field */}
                     <label 
@@ -188,16 +206,17 @@ const EditWarehouse = () => {
                     >Email</label>
                     <input 
                         name="email"
-                        className="form__input"
-                        placeholder={selectedWarehouse.contact.email}
-                        value={state.contact.email}
+                        className={!state?.contact.email?"form__input form__input--invalid": "form__input"}
+                        placeholder={state?.contact.email}
+                        value={state?.contact.email}
                         onChange={handleChangeContact}
                     ></input>
+                    <div className={!state?.contact.email?"invalid": "valid"}>This field is required</div>
                 </section>
             </div>
             <div  className='button-container'>
-                <button className="form__cancel-button" onClick={handleCancel}>Cancel</button>
-                <button className="form__save-button">Save</button>
+                <button type="reset" className="form__cancel-button" onClick={handleCancel}>Cancel</button>
+                <button type="submit" className="form__save-button">Save</button>
             </div>
         </form>
     </section>
