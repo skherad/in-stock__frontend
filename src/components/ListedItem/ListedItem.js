a773d38594cd32eadd546268cc78239fdee4f4ec
Michael Rideout <rideout.m@hotmail.com>
2022-09-12 11:28:15 -0400

    add inventory pg styles

--- src/components/ListedItem/ListedItem.js
+++ src/components/ListedItem/ListedItem.js
@@ -1,52 +1,61 @@
-import './ListedItem.scss'
-import lii from "../../assets/Icons/chevron_right-24px.svg"
-import Trash from '../../assets/Icons/delete_outline-24px.svg'
-import Edit from '../../assets/Icons/edit-24px.svg'
-import {Link} from "react-router-dom"
+import "./ListedItem.scss";
+import lii from "../../assets/Icons/chevron_right-24px.svg";
+import Trash from "../../assets/Icons/delete_outline-24px.svg";
+import Edit from "../../assets/Icons/edit-24px.svg";
+import { Link } from "react-router-dom";
 
+function ListedItem({ key, name, category, status, quantity, warehouseName }) {
+  // console.log(catagory)
+  return (
+    <div className="item-box">
+      <div className="item-box__lg">
+        <div className="item-box__in-left">
+          <h4 className="item-box__title">INVENTORY ITEM</h4>
 
-function ListedItem ({key, name, category, status, quantity, warehouseName}) {
-    // console.log(catagory)
-    return(
-        <div className="item-box">
-            <div className='item-box__lg'>
-                <div className='item-box__in'>
-                    <p className='item-box__title'>INVENTORY ITEM</p>
-                    <Link className='item-box__link' to='/item/:id'>
-                    <div className='item-box__li'>
-                        <p className='item-box__i-name'>{name}</p>
-                        <img className='item-bo__ri' src={lii}></img>
-                    </div>
-                    </Link>
-                </div>
-                <div className='item-box__in'>
-                    <p className='item-box__title'>CATAGORY</p>
-                    <p className='item-box__c-cat'>{category}</p>   
-                </div>
+          <div className="item-box__li">
+            <div className="item-box__i-name">
+              <Link className="item-box__link" to="/item/:id">
+                {" "}
+                {name}
+                <img className="item-box__ri" src={lii}></img>
+              </Link>
             </div>
-            <div className='item-box__rg'>
-                <div className='item-box__in'>
-                    <p className='item-box__title'>STATUS</p>
-                    <p className={`item-box__${status ? 'In-Stock' : 'Out-of-Stock'}`}>{status}</p>
-                </div>
-                <div className='item-box__in'>
-                    <p className='item-box__title'>QTY</p>
-                    <p className='item-box__input'>{quantity}</p>
-                </div>
-                <div className='item-box__in'>
-                    <p className='item-box__title'>WAREHOUSE</p>
-                    <p className='item-box__input'>{warehouseName}</p>
-                </div>
-            </div>
-            <div className='item-box__icg'>
-                <Link className='item-box__link' to='/delete'>
-                <img className='item-box__tcon' src={Trash}></img>
-                </Link>
-                <Link className='item-box__link' to='/edit'>
-                <img className='item-box__econ' src={Edit}></img>
-                </Link>       
-            </div>    
+          </div>
         </div>
-    )
+        <div className="item-box__in-left">
+          <h4 className="item-box__title">CATEGORY</h4>
+          <p className="item-box__c-cat">{category}</p>
+        </div>
+      </div>
+      <div className="item-box__rg">
+        <div className="item-box__in-right">
+          <h4 className="item-box__title">STATUS</h4>
+          <span
+            className={`item-box__${
+              quantity !== 0 ? "In-Stock" : "Out-of-Stock"
+            }`}
+          >
+            {quantity !== 0 ? "IN STOCK" : " OUT OF STOCK"}
+          </span>
+        </div>
+        <div className="item-box__in-right">
+          <h4 className="item-box__title">QTY</h4>
+          <p className="item-box__input-qty">{quantity}</p>
+        </div>
+        <div className="item-box__in-right">
+          <h4 className="item-box__title">WAREHOUSE</h4>
+          <p className="item-box__input">{warehouseName}</p>
+        </div>
+      </div>
+      <div className="item-box__icg">
+        <Link className="item-box__link" to="/delete">
+          <img className="item-box__tcon" src={Trash}></img>
+        </Link>
+        <Link className="item-box__link" to="/edit">
+          <img className="item-box__econ" src={Edit}></img>
+        </Link>
+      </div>
+    </div>
+  );
 }
-export default ListedItem
+export default ListedItem;
--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,20 +1,17 @@
-import './Inventory.scss';
+import "./Inventory.scss";
 //icons
-import Search from '../../assets/Icons/search-24px.svg';
-import Sort from '../../assets/Icons/sort-24px.svg'
-import Invin from '../../assets/data/inventories.json'
-import ListedItem from '../../components/ListedItem/ListedItem';
+import Search from "../../assets/Icons/search-24px.svg";
+import Sort from "../../assets/Icons/sort-24px.svg";
+import Invin from "../../assets/data/inventories.json";
+import ListedItem from "../../components/ListedItem/ListedItem";
 
-import { useState, useEffect } from "react"
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
-
-function Inventory () {
-
-
-  const [currWarehouse, setCurrWarehouse] = useState(getData(Invin))
-  console.log(currWarehouse)
+function Inventory() {
+  const [currWarehouse, setCurrWarehouse] = useState(getData(Invin));
+  console.log(currWarehouse);
 
   // useEffect(()=> {
   //   axios.get(`${API_URL}/warehouse`)
@@ -23,78 +20,85 @@ function Inventory () {
   //     // setCurrWarehouse(res)
   //   })
   // }, [])
-  function getData(array){
-    const find = array.filter(x=> x.warehouseID==="2922c286-16cd-4d43-ab98-c79f698aeab0")
-    return find
+  function getData(array) {
+    const find = array.filter(
+      (x) => x.warehouseID === "2922c286-16cd-4d43-ab98-c79f698aeab0"
+    );
+    return find;
   }
-    // console.log(getData(Invin))
+  // console.log(getData(Invin))
   // console.lo g(currWarehouse)
 
-useEffect(() => {
-  
-})
+  useEffect(() => {});
 
-
-    return (
-      //This is the main container
-      <div className='container'>
-        {/* this is the container that will hold all content that gets displayed */}
-        <div className='invientory'>
-          <div className='invientory__head'>
-            <h1 className='invientory__title'>Inventory</h1>
-            <div className='invientory__ui-head'>
-              <div className='invientory__search'>
-                <input className='invientory__input' type="text" size='8' placeholder='Search...'></input>
-                <img className='invientory__s-icon' src={Search} alt="magnifying glass"></img>
-              </div>
-              <div className='invientory__add-btn'>
-                <button className='invientory__btn'>+ Add New Item</button>
-              </div>
+  return (
+    //This is the main container
+    <div className="container">
+      {/* this is the container that will hold all content that gets displayed */}
+      <div className="invientory">
+        <div className="invientory__head">
+          <h1 className="invientory__title">Inventory</h1>
+          <div className="invientory__ui-head">
+            <div className="invientory__search">
+              <input
+                className="invientory__input"
+                type="text"
+                size="25"
+                placeholder="Search..."
+              ></input>
+              <img
+                className="invientory__s-icon"
+                src={Search}
+                alt="magnifying glass"
+              ></img>
+            </div>
+            <div className="invientory__add-btn">
+              <button className="invientory__btn">+ Add New Item</button>
             </div>
           </div>
-          <section className='invientory__header-catagory'>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>INVENTORY ITEM</h3>
-              <img className='invientory__cat-pic' src={Sort}></img>
-            </div>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>CATAGORY</h3>
-              <img className='invientory__cat-pic' src={Sort}></img>
-            </div>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>STATUS</h3>
-              <img className='invientory__cat-pic' src={Sort}></img>
-            </div>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>QTY</h3>
-              <img className='invientory__cat-pic' src={Sort}></img>
-            </div>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>WAREHOUSE</h3>
-              <img className='invientory__cat-pic' src={Sort}></img>
-            </div>
-            <div className='invientory__header-cat'>
-              <h3 className='invientory__cat-i'>ACTION</h3>
-            </div>
-          </section>
-          <section className='invientory__list'>
-            {currWarehouse.map((x)=>{
-              return(
-                <ListedItem
-                key = {x.id}
-                name = {x.itemName}
-                category = {x.category}
-                status = {x.status}
-                quantity = {x.quantity}
-                warehouseName = {x.warehouseName}
-                />
-              )
-              
-            })}
-          </section>
         </div>
+        <section className="invientory__header-catagory">
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">INVENTORY ITEM</h3>
+            <img className="invientory__cat-pic" src={Sort}></img>
+          </div>
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">CATEGORY</h3>
+            <img className="invientory__cat-pic" src={Sort}></img>
+          </div>
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">STATUS</h3>
+            <img className="invientory__cat-pic" src={Sort}></img>
+          </div>
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">QTY</h3>
+            <img className="invientory__cat-pic" src={Sort}></img>
+          </div>
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">WAREHOUSE</h3>
+            <img className="invientory__cat-pic" src={Sort}></img>
+          </div>
+          <div className="invientory__header-cat">
+            <h3 className="invientory__cat-i">ACTIONS</h3>
+          </div>
+        </section>
+        <section className="invientory__list">
+          {currWarehouse.map((x) => {
+            return (
+              <ListedItem
+                key={x.id}
+                name={x.itemName}
+                category={x.category}
+                status={x.status}
+                quantity={x.quantity}
+                warehouseName={x.warehouseName}
+              />
+            );
+          })}
+        </section>
       </div>
-    );
-  }
-  
-  export default Inventory;
+    </div>
+  );
+}
+
+export default Inventory;
