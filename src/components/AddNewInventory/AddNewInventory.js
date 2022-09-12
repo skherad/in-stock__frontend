c0830393e68bfdf93cd22cb4bc89531776bffa15
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-12 15:31:13 -0400

    Complete tablet and desktop styling for add inventory

--- src/components/AddNewInventory/AddNewInventory.js
+++ src/components/AddNewInventory/AddNewInventory.js
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { v4 as uuid } from 'uuid';
 import './AddNewInventory.scss';
-import backIcon from '../../assets/Icons/arrow_back-24px.svg'
+import backIcon from '../../assets/Icons/arrow_back-24px.svg';
+import ErrorMsg from '../ErrorMsg/ErrorMsg';
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
@@ -24,10 +25,10 @@ function AddNewInventory() {
     // form field states
     const [ itemName, setItemName ] = useState("");
     const [ description, setDescription ] = useState("");
-    const [ category, setCategory ] = useState("Please select");
+    const [ category, setCategory ] = useState("");
     const [ status, setStatus ] = useState("In Stock");
     const [ quantity, setQuantity ] = useState(0);
-    const [ warehouseName, setWarehouseName ] = useState("Please select");
+    const [ warehouseName, setWarehouseName ] = useState("");
 
     // form field value change functions
     const changeName = (event) => {
@@ -58,8 +59,8 @@ function AddNewInventory() {
     const showQuantity = () => {
         return (
             <label className="add-form__label">Quantity
-                <input className="form-field add-form__input" type="text" placeholder="Quantity" value={quantity} onChange={changeQuantity} />
-                <div className={!quantity && submitted ? "invalid" : "valid"}>This field is required</div>
+                <input className="form-field add-form__input add-form__input--quantity" type="text" placeholder="Quantity" value={quantity} onChange={changeQuantity} />
+                {status==="In Stock" && quantity===0 && submitted && <ErrorMsg />} {/*show error message if input invalid*/}
             </label>
         );
     }
@@ -73,13 +74,18 @@ function AddNewInventory() {
     const handleAdd = (event) => {
         event.preventDefault();
         
-        if (itemName && description && category && status && quantity && warehouseName) {
+        if (itemName && description && category && warehouseName && !(status==="In Stock" && quantity===0)) {
             let warehouseId;
 
             // find corresponding warehouse id if already exists in warehouse data
             axios.get(API_URL+"/warehouse")
                 .then((response) => warehouseId = response.data.filter((warehouse) => warehouse.name === warehouseName)?.id)
                 .catch((error) => console.log(error));
+
+            // set quantity to 0 if out of stock
+            if (status==="Out Of Stock") {
+                setQuantity(0);
+            }
 
             // request header
             const header = {
@@ -99,7 +105,7 @@ function AddNewInventory() {
 
             axios.post(API_URL+"/inventory", newItem, header)
                 .then(() => {
-                    handleBack();
+                    handleBack(); // navigate back to inventory list on successful add
                 }).catch((error) => console.log(error));
         } else {
             setSubmitted(true);
@@ -114,45 +120,52 @@ function AddNewInventory() {
                     <h1 className="add-header__title">Add New Inventory Item</h1>
                 </div>
                 <form className="add-form">
-                    <div className="add-form__subform">
-                        <h2 className="add-form__subheader">Item Details</h2>
-                        <label className="add-form__label">Item Name
-                            <input className="form-field add-form__input" type="text" name="itemName" placeholder="Item Name" value={itemName} onChange={changeName} />
-                            <div className={!itemName && submitted ? "invalid" : "valid"}>This field is required</div>
-                        </label>
-                        <label className="add-form__label">Description
-                            <textarea className="form-field add-form__input add-form__input--textarea" name="description" placeholder="Please enter a brief item description..." value={description} onChange={changeDescription}></textarea>
-                            <div className={!description && submitted ? "invalid" : "valid"}>This field is required</div>
-                        </label>
-                        <label className="add-form__label">Category
-                            <select className="form-field add-form__input" name="category" value={category} onChange={changeCategory}>
-                                {/* <option value="none" selected disabled hidden>Please select</option> */}
-                                <option value="electronics">Electronics</option>
-                                <option value="gear">Gear</option>
-                                <option value="apparel">Apparel</option>
-                                <option value="accesories">Accesories</option>
-                                <option value="health">Health</option>
-                            </select>
-                            <div className={!category && submitted ? "invalid" : "valid"}>This field is required</div>
-                        </label>
-                    </div>
-                    <div className="add-form__subform">
-                        <h2 className="add-form__subheader">Item Availability</h2>
-                        <label className="add-form__label">Status
-                            <div className="add-form__radios">
-                                <input className="add-form__radio" type="radio" id="in-stock" name="status" value="In Stock" checked={status==="In Stock"} onChange={changeStatus} />
-                                <label className="add-form__radio-label" htmlFor="in-stock">In stock</label> 
-                                <input className="add-form__radio" type="radio" id="out-of-stock" name="status" value="Out Of Stock" checked={status==="Out Of Stock"} onChange={changeStatus} />
-                                <label className="add-form__radio-label" htmlFor="out-of-stock">Out of stock</label>    
-                            </div>
-                        </label>
-                        {status==="In Stock" ? showQuantity() : null}
-                        <label className="add-form__label">Warehouse
-                            <select className="form-field add-form__input" name="warehouse" value={warehouseName} onChange={changeWarehouseName}>
-                                {warehouses.map((warehouse) => <option value={warehouse} key={uuid()}>{warehouse}</option>)}
-                            </select>
-                            <div className={!warehouseName && submitted ? "invalid" : "valid"}>This field is required</div>
-                        </label>
+                    <div className="add-form__subforms">
+                        <div className="add-form__subform">
+                            <h2 className="add-form__subheader">Item Details</h2>
+                            <label className="add-form__label">Item Name
+                                <input className="form-field add-form__input" type="text" name="itemName" placeholder="Item Name" value={itemName} onChange={changeName} />
+                                {!itemName && submitted && <ErrorMsg />} {/*show error message if input invalid*/}
+                            </label>
+                            <label className="add-form__label">Description
+                                <textarea className="form-field add-form__input add-form__input--textarea" name="description" placeholder="Please enter a brief item description..." value={description} onChange={changeDescription}></textarea>
+                                {!description && submitted && <ErrorMsg />} {/*show error message if input invalid*/}
+                            </label>
+                            <label className="add-form__label">Category
+                                <select className="form-field add-form__input add-form__input--select" name="category" value={category} onChange={changeCategory} required>
+                                    <option value="" disabled hidden>Please select</option>
+                                    <option value="Electronics">Electronics</option>
+                                    <option value="Gear">Gear</option>
+                                    <option value="Apparel">Apparel</option>
+                                    <option value="Accessories">Accessories</option>
+                                    <option value="Health">Health</option>
+                                </select>
+                                {!category && submitted && <ErrorMsg />} {/*show error message if input invalid*/}
+                            </label>
+                        </div>
+                        <div className="add-form__subform">
+                            <h2 className="add-form__subheader">Item Availability</h2>
+                            <label className="add-form__label">Status
+                                <div className="add-form__radios">
+                                    <div className="add-form__radio-container">
+                                        <input className="add-form__radio" type="radio" id="in-stock" name="status" value="In Stock" checked={status==="In Stock"} onChange={changeStatus} />
+                                        <label className="add-form__radio-label" htmlFor="in-stock">In stock</label> 
+                                    </div>
+                                    <div className="add-form__radio-container">
+                                        <input className="add-form__radio" type="radio" id="out-of-stock" name="status" value="Out Of Stock" checked={status==="Out Of Stock"} onChange={changeStatus} />
+                                        <label className="add-form__radio-label" htmlFor="out-of-stock">Out of stock</label>    
+                                    </div>
+                                </div>
+                            </label>
+                            {status==="In Stock" ? showQuantity() : null}
+                            <label className="add-form__label">Warehouse
+                                <select className="form-field add-form__input add-form__input--select" name="warehouse" value={warehouseName} onChange={changeWarehouseName} required>
+                                    <option value="" disabled hidden>Please select</option>
+                                    {warehouses.map((warehouse) => <option value={warehouse} key={uuid()}>{warehouse}</option>)}
+                                </select>
+                                {!warehouseName && submitted && <ErrorMsg />} {/*show error message if input invalid*/}
+                            </label>
+                        </div>
                     </div>
                     <div className="add-form__buttons">
                         <button className="button add-form__button add-form__button--cancel" onClick={handleBack}>Cancel</button>
