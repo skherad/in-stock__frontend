4137533aba5b97a42a0e822e94e3233ff74b2575
Irene Xing <irenexing0220@gmail.com>
2022-09-12 16:45:52 -0400

    modal styling

--- src/components/InventoryModal/InventoryModal.js
+++ src/components/InventoryModal/InventoryModal.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { useState } from 'react';
-import ReactDom from 'react-dom';
+import './InventoryModal.scss'
+import axios from 'axios';
 
-function InventoryModal ({openModal, closeModal, DeleteInventory}) {
+
+function InventoryModal ({openModal, closeModal, itemToDelete}) {
 if(!openModal) return null; 
+
 
 const handleSubmit = (event, id) => {
   event.preventDefault();
       axios.delete(`http://localhost:8080/inventory/${id}`)
         .then(response => {
-          console.log(response.data);
+          // console.log(response.data);
+          closeModal();
         })
         .catch(err => {
           console.error(err);
         });
     };
     
-    return ReactDom.createPortal(
+    return(
       <>
     <div className="modal__overlay-style"> </div>
     <section className="modal">
-        <div className="modal__title"> Delete {DeleteInventory.itemName} inventory item?</div>
-        <p className="modal__description">Please confirm that you'd like to delete {DeleteInventory.itemName} from the inventory list. <br> You won't be able to undo this action.</br></p>
+        <div className="modal__top"> 
+        <div className="modal__title"> Delete {itemToDelete.name} inventory item?</div>
+        <p className="modal__description">Please confirm that you'd like to delete {itemToDelete.name} from the inventory list.  You won't be able to undo this action.</p>
+        </div>
         <div className="modal__button">
         <button className="modal__button-cancel" onClick={closeModal}>Cancel</button>
         <button className="modal__button-delete" onClick={handleSubmit}>Delete</button>
         </div>
     </section>
-    </>,
-    document.getElementById('portal')
+    </>
     )
 }
 
